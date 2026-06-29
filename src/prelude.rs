af016c1dc5a9f3841aec59779439d17691912aeb
Kevin Phan <kevin2006phan@gmail.com>
2026-06-30 06:41:23 +1000

    create prelude for lib

---
+++ src/prelude.rs
@@ -0,0 +1,5 @@
+pub use crate::app::{App, SystemStage};
+pub use crate::plugin::Plugin;
+pub use crate::resources::Resources;
+
+pub use crate::system::{Commands, IntoSystem, Query, Res, ResMut, System};
