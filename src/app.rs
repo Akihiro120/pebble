9ef06b08afceecd23813c820d1fd2590f2cbd361
Kevin Phan <kevin2006phan@gmail.com>
2026-06-30 05:35:59 +1000

    added resource adding, and improved bounds on system add

--- src/app.rs
+++ src/app.rs
@@ -1,38 +1,54 @@
 use crate::{
     plugin::Plugin,
+    resources::Resources,
     system::{IntoSystem, System},
 };
 
 pub struct App {
-    pub world: hecs::World,
+    pub(crate) world: hecs::World,
+    pub(crate) resources: Resources,
+    plugins: Vec<Box<dyn Plugin>>,
     systems: Vec<Box<dyn System>>,
 }
 
 impl App {
     pub fn new() -> Self {
+        let mut world = hecs::World::default();
+        let resources = Resources::new(&mut world);
+
         Self {
-            world: hecs::World::default(),
+            world: world,
+            resources: resources,
+            plugins: Vec::new(),
             systems: Vec::new(),
         }
     }
 
     pub fn add_plugin(&mut self, plugin: impl Plugin) -> &mut Self {
-        plugin.build(self);
+        self.plugins.push(Box::new(plugin));
         self
     }
 
-    pub fn add_resource(&mut self) -> &mut Self {
+    pub fn add_resource(&mut self, res: impl hecs::Component) -> &mut Self {
+        self.resources.insert_resource(&mut self.world, res);
         self
     }
 
-    pub fn add_system<Marker>(&mut self, system: impl IntoSystem<Marker>) -> &mut Self {
+    pub fn add_system<Marker>(&mut self, system: impl IntoSystem<Marker> + 'static) -> &mut Self {
         self.systems.push(Box::new(system.into_system()));
+        self
+    }
+
+    pub fn build(&mut self) -> &mut App {
+        while let Some(plugin) = self.plugins.pop() {
+            plugin.build(self);
+        }
         self
     }
 
     pub fn update(&mut self) {
         for system in self.systems.iter_mut() {
-            system.run(&self.world);
+            system.run(&self.world, &self.resources);
         }
     }
 
@@ -40,10 +56,6 @@ impl App {
     where
         F: FnOnce(&mut App),
     {
-        // for plugin in &self.plugins {
-        //     plugin.build(&mut self);
-        // }
-
         runner(self);
     }
 }
