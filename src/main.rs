9878c6dac1f42abc5a98df3285f2eae1802b0832
Kevin Phan <kevin2006phan@gmail.com>
2026-06-30 05:36:05 +1000

    created example

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,47 @@
-use crate::{app::App, plugin::Plugin};
+use crate::{
+    app::App,
+    plugin::Plugin,
+    system::{Res, ResMut},
+};
 
 mod app;
 mod commands;
 mod plugin;
+mod resources;
 mod system;
 
-struct HelloWorldPlugin;
+#[derive(Default)]
+struct Time {
+    pub time: f32,
+}
 
-impl Plugin for HelloWorldPlugin {
+struct TimePlugin;
+
+impl Plugin for TimePlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(hello_world);
+        app.add_resource(Time::default())
+            .add_system(update_time)
+            .add_system(display_time);
     }
 }
 
-fn hello_world() {
-    println!("Hello, World! from the HelloWorldPlugin");
+fn update_time(mut time: ResMut<Time>) {
+    time.time += 1.0;
+}
+
+fn display_time(time: Res<Time>) {
+    println!("{}", time.time);
 }
 
 fn main() {
     App::new()
-        .add_plugin(HelloWorldPlugin {})
+        .add_plugin(TimePlugin {})
+        .build()
         .run(run_application);
 }
 
 fn run_application(app: &mut App) {
-    app.update();
+    for _ in 0..6 {
+        app.update();
+    }
 }
