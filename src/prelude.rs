5a7bf5968b606efff98b8866c363574bcff9d768
Kevin Phan <kevin2006phan@gmail.com>
2026-07-10 19:19:09 +1000

    removed plugin

--- src/prelude.rs
+++ src/prelude.rs
@@ -19,7 +19,3 @@ pub use crate::resources::Resources;
 pub use crate::system::{Commands, IntoSystem, Query, Res, ResMut, System};
 
 pub use crate::assets::asset_plugin::DeviceAssetPlugin;
-pub use crate::assets::dependent_asset_plugin::{
-    DependentAssetPlugin1, DependentAssetPlugin2, DependentAssetPlugin3, DependentAssetPlugin4,
-    DependentUpload1, DependentUpload2, DependentUpload3, DependentUpload4,
-};
