fca6390fd2fc3b28be08911266ad6bc8d2726fd5
Kevin Phan <kevin2006phan@gmail.com>
2026-07-16 23:48:50 +1000

    refactor: unified web and native backends

--- src/rendering/backend.rs
+++ src/rendering/backend.rs
@@ -1,17 +1,80 @@
-use crate::prelude::GPUSurfaceHandle;
+use crate::{errors::AcquireError, prelude::GPUSurfaceHandle, rendering::sync::InitSender};
+
+pub struct Pass<'a, F: FrameOperations + ?Sized> {
+    pub colors: &'a [ColorTarget<'a, F>],
+    pub depth: Option<DepthTarget<'a, F>>,
+}
 
 pub trait FrameOperations: Sync + Send + 'static {
     type Context<'a>;
-    fn context(&mut self) -> Self::Context<'_>;
+    type Attachment;
+    type DepthAttachment;
+
+    fn default_target(&self) -> &Self::Attachment;
+    fn begin(&mut self, pass: Pass<'_, Self>) -> Self::Context<'_>;
+}
+
+pub enum ColorTarget<'a, F: FrameOperations + ?Sized> {
+    Default {
+        clear: Option<[f32; 4]>,
+    },
+    Custom {
+        attachment: &'a F::Attachment,
+        clear: Option<[f32; 4]>,
+    },
+}
+
+impl<'a, F: FrameOperations> ColorTarget<'a, F> {
+    pub fn default(clear: [f32; 4]) -> Self {
+        Self::Default { clear: Some(clear) }
+    }
+    pub fn default_load() -> Self {
+        Self::Default { clear: None }
+    }
+    pub fn custom(attachment: &'a F::Attachment, clear: [f32; 4]) -> Self {
+        Self::Custom {
+            attachment,
+            clear: Some(clear),
+        }
+    }
+    pub fn custom_load(attachment: &'a F::Attachment) -> Self {
+        Self::Custom {
+            attachment,
+            clear: None,
+        }
+    }
+}
+
+pub struct DepthTarget<'a, F: FrameOperations + ?Sized> {
+    pub attachment: &'a F::DepthAttachment,
+    pub clear: Option<f32>,
+}
+
+impl<'a, F: FrameOperations> DepthTarget<'a, F> {
+    pub fn new(attachment: &'a F::DepthAttachment, clear: f32) -> Self {
+        Self {
+            attachment,
+            clear: Some(clear),
+        }
+    }
+    pub fn load(attachment: &'a F::DepthAttachment) -> Self {
+        Self {
+            attachment,
+            clear: None,
+        }
+    }
 }
 
 pub trait Backend: Sized + Sync + Send + 'static {
     type Frame: FrameOperations;
 
-    fn init(handle: impl GPUSurfaceHandle, width: u32, height: u32) -> Self;
-    fn resize(&mut self, width: u32, height: u32);
+    fn init(handle: impl GPUSurfaceHandle, width: u32, height: u32, sender: InitSender<Self>);
+    fn resize(&mut self, width: u32, height: u32) {
+        width;
+        height;
+    }
 
-    fn acquire(&mut self) -> Option<Self::Frame>;
+    fn acquire(&mut self) -> Result<Self::Frame, AcquireError>;
     fn present(&mut self, frame: Self::Frame);
 }
 
@@ -32,7 +95,20 @@ impl<B: Backend> CurrentFrame<B> {
         self.frame.is_some()
     }
 
-    pub fn get_render_context(&mut self) -> Option<<B::Frame as FrameOperations>::Context<'_>> {
-        self.frame.as_mut().map(|frame| frame.context())
+    pub fn render_context(
+        &mut self,
+        clear: [f32; 4],
+    ) -> Option<<B::Frame as FrameOperations>::Context<'_>> {
+        self.begin_pass(Pass {
+            colors: &[ColorTarget::Default { clear: Some(clear) }],
+            depth: None,
+        })
+    }
+
+    pub fn begin_pass(
+        &mut self,
+        pass: Pass<B::Frame>,
+    ) -> Option<<B::Frame as FrameOperations>::Context<'_>> {
+        self.frame.as_mut().map(|f| f.begin(pass))
     }
 }
--- src/rendering/web_backend.rs
+++ src/rendering/web_backend.rs
@@ -1,78 +1,12 @@
 use crate::{
-    prelude::{
-        Backend, Commands, GPUSurfaceHandle, Plugin, PresentableWindow, Res, ResMut, SystemStage,
-        WindowResource,
-    },
-    rendering::sync::{InitReceiver, InitSender, init_channel},
+    prelude::{Backend, GPUSurfaceHandle},
+    rendering::sync::{InitReceiver, InitSender},
 };
 
 pub trait AsyncInit: Backend {
     fn init_async(handle: impl GPUSurfaceHandle, width: u32, height: u32, sender: InitSender<Self>);
 }
 
-pub struct PendingBackend<B: AsyncInit> {
-    receiver: std::sync::Mutex<InitReceiver<B>>,
-}
-
-pub struct AsyncGraphicsPlugin<B, W>(std::marker::PhantomData<(B, W)>);
-
-impl<B, W> AsyncGraphicsPlugin<B, W> {
-    pub fn new() -> Self {
-        Self(std::marker::PhantomData)
-    }
-}
-
-impl<B: AsyncInit, W> Plugin for AsyncGraphicsPlugin<B, W>
-where
-    W: PresentableWindow,
-    W::Handle: GPUSurfaceHandle,
-{
-    fn build(&self, app: &mut crate::app::App) {
-        app.add_system(SystemStage::Startup, setup_gpu_async::<B, W>)
-            .add_system(SystemStage::PreRender, poll_backend_ready::<B>)
-            .add_system(SystemStage::PreRender, handle_resize_async::<B, W>);
-    }
-}
-
-fn setup_gpu_async<B: AsyncInit, W>(mut commands: Commands, window: Res<WindowResource<W>>)
-where
-    W: PresentableWindow,
-    W::Handle: GPUSurfaceHandle,
-{
-    let (w, h) = W::size(&window.handle);
-    let (sender, receiver) = init_channel::<B>();
-    B::init_async(window.handle.clone(), w, h, sender);
-    commands.insert_resource(PendingBackend::<B> {
-        receiver: std::sync::Mutex::new(receiver),
-    });
-}
-
-fn poll_backend_ready<B: AsyncInit>(
-    mut commands: Commands,
-    pending: Option<Res<PendingBackend<B>>>,
-) {
-    if let Some(p) = pending {
-        let mut guard = match p.receiver.lock() {
-            Ok(g) => g,
-            Err(poisoned) => poisoned.into_inner(),
-        };
-
-        if let Ok(backend) = guard.try_recv() {
-            commands.insert_resource(backend);
-            commands.remove_resource::<PendingBackend<B>>();
-        }
-    }
-}
-
-fn handle_resize_async<B: AsyncInit, W: PresentableWindow>(
-    backend: Option<ResMut<B>>,
-    window: Res<WindowResource<W>>,
-) where
-    W::Handle: GPUSurfaceHandle,
-{
-    let Some(mut backend) = backend else { return };
-    let (w, h) = W::size(&window.handle);
-    if w > 0 && h > 0 {
-        backend.resize(w, h);
-    }
+pub(crate) struct PendingBackend<B: Backend> {
+    pub(crate) receiver: std::sync::Mutex<InitReceiver<B>>,
 }
