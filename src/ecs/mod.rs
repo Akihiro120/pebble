6ca6a0c122394c4d34b19b683efda17ad45d1045
Kevin Phan <kevin2006phan@gmail.com>
2026-08-12 06:02:54 +1000

    feat: added command buffer for resources

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -9,9 +9,12 @@ pub mod system_param;
 
 #[cfg(test)]
 mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
     use crate::ecs::{
-        commands::Commands,
-        resources::{Resources, Write},
+        commands::{Commands, ResourceCommandQueue},
+        local::Local,
+        resources::{Read, Resources, Write},
         schedule::Schedule,
     };
 
@@ -72,8 +75,8 @@ mod tests {
             *counter += 1;
         });
 
-        schedule.run(&mut world, &resources);
-        schedule.run(&mut world, &resources);
+        schedule.run(&mut world, &mut resources);
+        schedule.run(&mut world, &mut resources);
 
         assert_eq!(*resources.get::<i32>(), 2);
     }
@@ -83,6 +86,7 @@ mod tests {
         let mut world = hecs::World::default();
         let mut resources = Resources::default();
         resources.insert(hecs::CommandBuffer::default());
+        resources.insert(ResourceCommandQueue::default());
 
         let mut schedule = Schedule::default();
         schedule.add_system(|mut commands: Commands| {
@@ -91,8 +95,169 @@ mod tests {
 
         assert_eq!(world.len(), 0);
 
-        schedule.run(&mut world, &resources);
+        schedule.run(&mut world, &mut resources);
 
         assert_eq!(world.len(), 1);
+    }
+
+    #[test]
+    fn commands_insert_resource_is_applied_on_sync() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+        resources.insert(ResourceCommandQueue::default());
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|mut commands: Commands| {
+            commands.insert_resource(42i32);
+        });
+
+        assert!(!resources.contains::<i32>());
+
+        schedule.run(&mut world, &mut resources);
+
+        assert_eq!(*resources.get::<i32>(), 42);
+    }
+
+    #[test]
+    fn commands_remove_resource_is_applied_on_sync() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+        resources.insert(ResourceCommandQueue::default());
+        resources.insert(42i32);
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|mut commands: Commands| {
+            commands.remove_resource::<i32>();
+        });
+
+        schedule.run(&mut world, &mut resources);
+
+        assert!(!resources.contains::<i32>());
+    }
+
+    #[test]
+    fn option_read_is_some_when_resource_present() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+        resources.insert(42i32);
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|value: Option<Read<i32>>| {
+            assert_eq!(*value.expect("resource should be present"), 42);
+        });
+
+        schedule.run(&mut world, &mut resources);
+    }
+
+    #[test]
+    fn option_read_is_none_when_resource_missing() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|value: Option<Read<i32>>| {
+            assert!(value.is_none());
+        });
+
+        schedule.run(&mut world, &mut resources);
+    }
+
+    #[test]
+    fn option_write_mutates_resource_when_present() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+        resources.insert(1i32);
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|value: Option<Write<i32>>| {
+            *value.expect("resource should be present") += 1;
+        });
+
+        schedule.run(&mut world, &mut resources);
+
+        assert_eq!(*resources.get::<i32>(), 2);
+    }
+
+    #[test]
+    fn option_write_is_none_when_resource_missing() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|value: Option<Write<i32>>| {
+            assert!(value.is_none());
+        });
+
+        schedule.run(&mut world, &mut resources);
+    }
+
+    #[test]
+    fn local_defaults_to_type_default() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(|counter: Local<i32>| {
+            assert_eq!(*counter, 0);
+        });
+
+        schedule.run(&mut world, &mut resources);
+    }
+
+    #[test]
+    fn local_persists_across_runs() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+
+        let log = Rc::new(RefCell::new(Vec::new()));
+        let log_clone = log.clone();
+
+        let mut schedule = Schedule::default();
+        schedule.add_system(move |mut counter: Local<i32>| {
+            *counter += 1;
+            log_clone.borrow_mut().push(*counter);
+        });
+
+        schedule.run(&mut world, &mut resources);
+        schedule.run(&mut world, &mut resources);
+        schedule.run(&mut world, &mut resources);
+
+        assert_eq!(*log.borrow(), vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn local_state_is_isolated_per_system() {
+        let mut world = hecs::World::default();
+        let mut resources = Resources::default();
+        resources.insert(hecs::CommandBuffer::default());
+
+        let log_a = Rc::new(RefCell::new(Vec::new()));
+        let log_b = Rc::new(RefCell::new(Vec::new()));
+        let (la, lb) = (log_a.clone(), log_b.clone());
+
+        let mut schedule = Schedule::default();
+        schedule
+            .add_system(move |mut counter: Local<i32>| {
+                *counter += 1;
+                la.borrow_mut().push(*counter);
+            })
+            .add_system(move |mut counter: Local<i32>| {
+                *counter += 10;
+                lb.borrow_mut().push(*counter);
+            });
+
+        schedule.run(&mut world, &mut resources);
+        schedule.run(&mut world, &mut resources);
+
+        assert_eq!(*log_a.borrow(), vec![1, 2]);
+        assert_eq!(*log_b.borrow(), vec![10, 20]);
     }
 }
